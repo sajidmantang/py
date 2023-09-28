7280c72b3ae3db5f6ca6518d472e0ca8e6991ba2
sajidmantang <79866208+sajidmantang@users.noreply.github.com>
2023-09-28 22:29:58 +0700

    Update ex_gradient_descent.py

--- ML/3_gradient_descent/Exercise/ex_gradient_descent.py
+++ ML/3_gradient_descent/Exercise/ex_gradient_descent.py
@@ -11,7 +11,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import math
 
-def predict_using_sklern():
+def predict_using_sklearn():
     df = pd.read_csv("test_scores.csv")
     r = LinearRegression()
     r.fit(df[['math']],df.cs)
@@ -48,6 +48,6 @@ if __name__ == "__main__":
     m, b = gradient_descent(x,y)
     print("Using gradient descent function: Coef {} Intercept {}".format(m, b))
 
-    m_sklearn, b_sklearn = predict_using_sklern()
+    m_sklearn, b_sklearn = predict_using_sklearn()
     print("Using sklearn: Coef {} Intercept {}".format(m_sklearn,b_sklearn))
 
